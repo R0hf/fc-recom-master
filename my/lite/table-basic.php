e1850b3c4ede3513b575c672dc19a8e571f6a169
mnsDZ <33765559+mnsDZ@users.noreply.github.com>
2018-06-10 20:34:18 +0100

    jj

--- my/lite/table-basic.php
+++ my/lite/table-basic.php
@@ -156,50 +156,29 @@ session_start();
                                         <thead>
                                             <tr>
                                                 <th>#</th>
-                                                <th>First Name</th>
-                                                <th>Last Name</th>
-                                                <th>Username</th>
+                                                
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            <?php  
+                                             include_once('../../connect/connection.php');
+                                             $id=$l['clientID'];
+                                            $resultat = mysqli_query($connect,"SELECT hotelID FROM favourite WHERE clientID='$id';");
+                                            
+
+                                             while ($row = mysqli_fetch_assoc($resultat)){ 
+                                                include_once('../../connect/connection.php');
+                                                $test=$row['hotelID']
+                                                $res=mysqli_query($connect,"SELECT name_hotel FROM hotel WHERE hotelID='$test;");
+                                                $row = mysqli_fetch_assoc($res);
+                                                ?>
                                             <tr>
-                                                <td>1</td>
-                                                <td>Deshmukh</td>
-                                                <td>Prohaska</td>
-                                                <td>@Genelia</td>
+                                                <td><?php echo$row['name_hotel']?></td>
+                                                
                                             </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>Deshmukh</td>
-                                                <td>Gaylord</td>
-                                                <td>@Ritesh</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>Sanghani</td>
-                                                <td>Gusikowski</td>
-                                                <td>@Govinda</td>
-                                            </tr>
-                                            <tr>
-                                                <td>4</td>
-                                                <td>Roshan</td>
-                                                <td>Rogahn</td>
-                                                <td>@Hritik</td>
-                                            </tr>
-                                            <tr>
-                                                <td>5</td>
-                                                <td>Joshi</td>
-                                                <td>Hickle</td>
-                                                <td>@Maruti</td>
-                                            </tr>
-                                            <tr>
-                                                <td>6</td>
-                                                <td>Nigam</td>
-                                                <td>Eichmann</td>
-                                                <td>@Sonu</td>
-                                            </tr>
+                                            
                                         </tbody>
-                                    </table>
+                                    </table><?php }?>
                                 </div>
                             </div>
                         </div>
