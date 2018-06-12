63fc4cea428c9ddb6696848835399f51d32322b3
raouf <titisym@gmail.com>
2018-06-12 04:00:25 +0100

    2

--- php/deleteFromP.php
+++ php/deleteFromP.php
@@ -1,11 +1,13 @@
 <?php
-
+	session_start();
 	include("../connect/connection.php") ;
 	
-	$HotId = $_POST['variable'];
-	$ClId = $_POST['alv'];
+	$HotId = $_GET['id'];
+	$ClId = $_SESSION['id'];
 
 	$sql = "DELETE FROM favourite WHERE hotelID = '$HotId' AND clientID = '$ClId'" ;
      $res=mysqli_query($connect,$sql );
+
+     header("location:../my/lite/table-basic.php") ;
 
   ?>
